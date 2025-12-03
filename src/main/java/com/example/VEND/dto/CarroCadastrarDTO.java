14480fa9ffd0da6580a2ac1f38fb45858468c7d5
Bernardo Kist Souza <bekist2006@gmail.com>
2025-12-03 18:31:19 -0300

    update dto

--- src/main/java/com/example/VEND/dto/CarroCadastrarDTO.java
+++ src/main/java/com/example/VEND/dto/CarroCadastrarDTO.java
@@ -1,11 +1,12 @@
 package com.example.VEND.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CarroCadastrarDTO(@NotBlank String carroceria,
-                                @NotBlank byte[] imagem,
+                                @NotNull byte[] imagem,
                                 @NotBlank String modelo,
-                                @NotBlank Integer ano,
-                                @NotBlank Double preco,
+                                @NotNull Integer ano,
+                                @NotNull Double preco,
                                 @NotBlank String marca) {
 }
